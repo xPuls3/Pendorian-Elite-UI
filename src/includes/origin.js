3f8fb5c9b64bd338eee6924b0cf46fe899ae39c9
Puls3 <xer0.puls3.xxii@gmail.com>
2020-04-11 04:14:08 -0400

    Slightly split the origin function

--- src/includes/origin.js
+++ src/includes/origin.js
@@ -3,41 +3,62 @@
     origin();
 
     function origin () {
+
         logDo(`Initiating Pendorian Elite UI v${version}`, 0);
+
         const k = Object.keys(modules);
+
         let promiseList = [];
         let isGame = checkIsPath("/game");
         let isLogin = checkIsPath("/");
+
         for (let i = 0; i < k.length; i++) {
+
             modules[k[i]].id = String(k[i]);
+
             if (typeof (modules[k[i]].name) === "undefined") modules[k[i]].name = modules[k[i]].id;
-            if (isGame || (isLogin && modules[k[i]].runLogin)) {
-                if (typeof (modules[k[i]].options) !== "undefined") {
-                    if (modules[k[i]].options.status) {
-                        if (typeof (modules[k[i]].code) !== "undefined") {
-                            promiseList.push(new Promise(function (resolve) {
-                                modules[k[i]].code(resolve);
-                            }).then(function (result) {
-                                if (result) {
-                                    modules[k[i]].style = result;
-                                    applyStyle(modules[k[i]]);
-                                }
-                                if (isDebug) {
-                                    logDo(`${modules[k[i]].name} Module enabled`, 0);
-                                }
-                            }));
-                        } else {
-                            logDo(`${modules[k[i]].name} Module does not contain code`, 1);
-                        }
+
+            if (isGame || (isLogin && modules[k[i]].runLogin)) activateModule(modules[k[i]]);
+
+        }
+
+        Promise.all(promiseList).then(function () {
+
+            if (isDebug) logDo("Script completely loaded", 0);
+
+        });
+
+        function activateModule(module) {
+            if (typeof (module.options) !== "undefined") {
+                if (module.options.status) {
+                    if (typeof (module.code) !== "undefined") {
+                        promiseList.push(new Promise(function (resolve) {
+                            module.code(resolve);
+                        }).then(function (result) {
+                            moduleEnabled(module, result)
+                        }));
+                    } else {
+                        logDo(`${module.name} Module does not contain code`, 1);
                     }
-                } else {
-                    logDo(`${modules[k[i]].name} Module does not contain options`, 1);
                 }
+            } else {
+                logDo(`${module.name} Module does not contain options`, 1);
             }
         }
-        Promise.all(promiseList).then(function () {
-            if (isDebug) logDo("Script completely loaded", 0);
-        });
+
+    }
+
+    function moduleEnabled (module, result) {
+
+        if (result) {
+            module.style = result;
+            applyStyle(module);
+        }
+
+        if (isDebug) {
+            logDo(`${module.name} Module enabled`, 0);
+        }
+
     }
 
     function applyStyle (module) {
