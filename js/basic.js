0076a5795e20d13829de98641bdb90e4cf660953
Puls3 <xer0.puls3.xxii@gmail.com>
2020-05-14 08:23:11 -0400

    Improvements for Google results

--- js/basic.js
+++ js/basic.js
@@ -1,5 +1,10 @@
 window.addEventListener("load", function () {
+
+    document.title = "Pendorian Elite UI";
+
     if (window.hasOwnProperty("eliteUI")) {
         document.querySelector("#theDownload").innerText = "Update";
     }
+
+
 });
